fc166f994f303208a39a04ff88e21f4a426516a2
Kia Yong <tankiayong91@gmail.com>
2015-10-24 00:21:49 +0800

    Add educator mailer for password reset

---
+++ app/mailers/educators/educator_mailer.rb
@@ -0,0 +1,9 @@
+class Educators::EducatorMailer < ApplicationMailer
+
+  def reset_password_email(user)
+    @name = user.name
+    @url = edit_password_reset_url(user.reset_password_token)
+    mail to: user.email, subject: 'Password Reset'
+  end
+
+end
