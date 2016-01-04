4ea99dffb18c3601f16b36ec86408f76c3541d59
Loh Chung Kit <ckloh.2012@sis.smu.edu.sg>
2016-01-04 16:38:38 +0800

    Added service folder and assign_access_code_service file: -Service to provide assign code logic processing
    Edited batch_users_controller:
    -Transferred processing logic to service file

--- app/controllers/educators/batch_users_controller.rb
+++ app/controllers/educators/batch_users_controller.rb
@@ -50,79 +50,15 @@ class Educators::BatchUsersController < Educators::BaseController
   end
 
   def assign
-    all_ac = AccessCode.all
-    email = params[:email]
-    users = User.all
-    user = nil
-    codeusage = nil
-    list = []
-    sum = 0 
-    all_ac.each do |ac|
-      if ac.educator_id == @educator.id && ac.test.id == params[:test].to_i
-        list.push(ac)
-        sum += ac.permits
-      end
-    end
-    
-    users.blank? ? "": users.each{|u| u.email == email ? user = u : ""}
-
-    list.each do |ac|
-      if ac.permits-ac.code_usages.size > 0
-        if user.blank?
-          codeusage = CodeUsage.new(access_code: ac, state: 1, batch_id: params[:id], email: email)
-          codeusage.save; return if performed?
-          flash[:notice ] = "Code Assigned"
-        else
-          codeusage = CodeUsage.new(access_code: ac, user_id: user.id, state: 1, batch_id: params[:id], email: email)
-          codeusage.save; return if performed?
-          flash[:notice ] = "Code Assigned"
-        end
-      end
-    end
+    acs = Educators::AssignAccessCodeService.new({ac:AccessCode.all,email:params[:email],users:User.all,educator:@educator,test_id:params[:test],batch_id:params[:id]})
+    acs.assign_code ? flash[:notice ] = "Code assigned" : flash[:notice ] = "Insufficient number of codes"
     redirect_to controller: "batches", action: "show", id: params[:id]
   end
 
   def assignall
-    all_ac = AccessCode.all
-    users = User.all
-    user = nil
-    list = []
-    sum = 0 
-    all_ac.each do |ac|
-      if ac.educator_id == @educator.id && ac.test.id == params[:test].to_i
-        list.push(ac)
-        sum = ac.permits - ac.code_usages.size
-      end
-    end
-
     batch = Batch.find(params[:batch])
-    email_list = batch.email
-    num = email_list.count
-
-    email_list.each {|el| CodeUsage.exists?(email: el, batch_id: batch.id) ? num -= 1 : ""}
-
-    testnumber = 0
-    if sum >= num
-      email_list.each do |el|
-        list.each do |ac|
-          if ac.permits-ac.code_usages.size > 0
-            users.blank? ? "": users.each{|u| u.email == el ? user = u : ""}
-            if user.blank?
-              unless CodeUsage.exists?(email: el, batch_id: batch.id) 
-                codeusage = CodeUsage.new(access_code: ac, state: 1, batch_id: batch.id, email: el)
-                codeusage.save; return if performed?
-              end
-            else
-              codeusage = CodeUsage.new(access_code: ac, user_id: user.id, state: 1, batch_id: batch.id, email: el)
-              codeusage.save; return if performed?
-            end
-          end
-        end
-      end
-      flash[:notice ] = "Code Assigned"
-    else
-      flash[:alert ] = "Not enough access codes"
-    end
+    acs = Educators::AssignAccessCodeService.new({ac:AccessCode.all, users:User.all, educator:@educator, test_id:params[:test], batch_id:batch.id})
+    acs.assign_to_all ? flash[:notice ] = "Codes assigned" : flash[:notice ] = "Insufficient number of codes"
     redirect_to controller: "batches", action: "show", id: params[:batch]
   end
 end
