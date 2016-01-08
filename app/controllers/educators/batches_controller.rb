39ec2751814c682ea22c56472489c3176cf50f78
jjemin <jjemin1@naver.com>
2016-01-09 05:48:17 +0800

    Modified batches_controller for new UI

--- app/controllers/educators/batches_controller.rb
+++ app/controllers/educators/batches_controller.rb
@@ -20,15 +20,20 @@ class Educators::BatchesController < Educators::BaseController
     @batch = Batch.new(educator: @educator, test: Test.find_by(id: @test_id), name: @batchname)
     if @batch.save
       flash[:notice ] = "Batch Created"
-      redirect_to educators_batches_path
+      redirect_to educators_educator_path(@educator)
+    else
+      flash[:notice ] = "Please enter Batch Name"
+      redirect_to educators_educator_path(@educator)
     end
   end
 
   def show
     if (params[:id]) 
       @batch = Batch.find(params[:id])
+      @remaining_access_codes = AccessCode.where(educator_id: @educator.id, test_id: @batch.test.id).count
     else  
       @batch = Batch.find(params[:upload_csv][:batch_id])
+      @remaining_access_codes = AccessCode.where(educator_id: @educator.id, test_id: @batch.test.id).count
       @list = @email_list.all
     end
   end
@@ -36,7 +41,14 @@ class Educators::BatchesController < Educators::BaseController
   def destroy
     Batch.find(params[:id]).destroy
     flash[:alert ] = "Batch Deleted"
-    redirect_to educators_batches_path
+    redirect_to educators_educator_path(@educator)
+  end
+
+  def add_batch
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
