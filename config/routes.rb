5261931ff7b57776ce36d3bf531e96256395070c
Mai Anh Vu <me@maianhvu.com>
2016-02-01 20:12:47 +0800

    remove API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,4 @@
 Rails.application.routes.draw do
-  scope module: 'api' do
-    resources :users, only: [ :create, :show ]
-    resources :tests, only: [ :index, :show ] do
-      resources :questions, only: [ :index ]
-      resources :answers,   only: [ :create ]
-      resources :results,   only: [ :index ]
-
-      get 'use-code/:code', to: 'access_codes#use', as: 'use_code'
-    end
-
-    post 'register', to: 'users#create', as: :register
-    post 'sign-in', to: 'users#sign_in'
-    post 'sign-out', to: 'users#sign_out'
-
-    constraints(escaped_email: /[^\/]+/) do
-      get 'confirm/:escaped_email/:token', to: 'users#confirm'
-    end
-
-    get 'profile', to: 'users#show'
-  end
 
   namespace :educators do
     resources :educators, only: [ :index, :new, :create, :show, :edit, :update ]
@@ -48,9 +28,5 @@ Rails.application.routes.draw do
 
     root to: 'educator_sessions#new'
   end
-
-  root to: redirect('http://app.starteur.com/')
-
-  match '*others', to: 'authenticated#allow', via: [ :options ]
 
 end
