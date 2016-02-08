30faba21d7afdea59ee1f42b59fe0d83892e0af7
Mai Anh Vu <me@maianhvu.com>
2016-02-08 11:06:25 +0800

    configure environment for sending mails with sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # ---------------------------------------------------------------------------------
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
   }, path_names: {
     sign_in: 'sign-in',
     sign_out: 'sign-out',
