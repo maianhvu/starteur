74ecf9b175dc267df1c6f1e9cb8c602df3cbc8ea
Mai Anh Vu <me@maianhvu.com>
2016-02-08 14:33:30 +0800

    add sign in tests to make sure confirmation behaviour works

--- spec/features/auth_confirmation_spec.rb
+++ spec/features/auth_confirmation_spec.rb
@@ -5,13 +5,14 @@ feature 'Authentication: Confirmation', type: :feature do
   scenario 'Confirming from email' do
     # Try to register for a new account first
     user = FactoryGirl.build(:user)
+    user_password = user.password
 
     expect {
       visit new_user_registration_path
       fill_in 'First name', with: user.first_name
       fill_in 'Last name', with: user.last_name
       fill_in 'Email', with: user.email
-      fill_in :user_password, with: user.password
+      fill_in :user_password, with: user_password
       click_button 'Register'
     }.to change { User.count }.by(1)
 
@@ -31,5 +32,13 @@ feature 'Authentication: Confirmation', type: :feature do
       current_email.click_link 'Confirm my account'
     }.to change { user.reload.state }.from('registered').to('confirmed')
     expect(user.confirmed_at).to be_within(5.seconds).of(Time.now)
+
+    # Should log in again, and succeed
+    expect(current_path).to eq(new_user_session_path)
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user_password
+    click_button 'Sign in'
+
+    expect(current_path).to eq(dashboard_index_path)
   end
 end
