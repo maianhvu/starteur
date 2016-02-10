3890dd99cc7b99cc58c4d55017cd90d75ddcc804
Mai Anh Vu <me@maianhvu.com>
2016-02-10 11:52:55 +0800

    finish refactoring results and answers to not aggregate

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,6 +5,7 @@ class Result < ActiveRecord::Base
   belongs_to :user
   belongs_to :code_usage
   has_and_belongs_to_many :batches
+  has_many :answers
 
   # Validations
   validates_presence_of :answers, :test, :user, :code_usage
