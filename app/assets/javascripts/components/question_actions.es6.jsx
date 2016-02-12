bca285ebcd273113efd93dff7cdf57b4c3cd8417
Mai Anh Vu <me@maianhvu.com>
2016-02-13 01:24:17 +0800

    add question actions to question box React component

--- app/assets/javascripts/components/question_actions.es6.jsx
+++ app/assets/javascripts/components/question_actions.es6.jsx
@@ -1,5 +1,20 @@
 class QuestionActions extends React.Component {
+
+  notifyParentNextQuestion() {
+    console.log('next question');
+  }
+
   render() {
-    return (<div>Hello, QuestionActions!</div>);
+    return (
+      <div className="question__actions">
+        <div className="question__next-button button ghost" onClick={this.notifyParentNextQuestion}>
+          Next <span className="chevron" />
+        </div>
+        <div className="question__minor-actions">
+          <div className="actions__skip-question">Skip, answer this question later</div>
+          <div className="actions__help">Need help?</div>
+        </div>
+      </div>
+    );
   }
 }
