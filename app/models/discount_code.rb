6966701a29497d99bc8f36e9d5b6f0d83d9fc501
Kia Yong <tankiayong91@gmail.com>
2016-02-17 13:25:45 +0800

    Add validation for discount and promotion codes

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -7,7 +7,7 @@ class DiscountCode < ActiveRecord::Base
   belongs_to :billing_record
 
   validates :percentage, presence: true
-  validates :percentage, numericality: { more_than_or_equal_to: 0, integer: true }
+  validates :percentage, numericality: { more_than: 0, less_than_or_equal_to: 100, integer: true }
 
   # State definitions
   enum state: {
--- app/models/promotion_code.rb
+++ app/models/promotion_code.rb
@@ -9,7 +9,7 @@ class PromotionCode < ActiveRecord::Base
   belongs_to :test
 
   validates :quantity, presence: true
-  validates :quantity, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than: 0, integer: true }
 
   # State definitions
   enum state: {
