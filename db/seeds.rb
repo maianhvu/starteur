9f67ba49d693ef126f21ed8ba8a629db8f3024c2
Kia Yong <tankiayong91@gmail.com>
2016-02-17 14:19:44 +0800

    Confirm seed users' emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 u = User.create(email: 'ckloh.2012@sis.smu.edu.sg', first_name: 'Chung Kit', last_name: 'Loh', password: '123123123')
+u.confirm_email!
 u2 = User.create(email: 'choryi.poon.2012@sis.smu.edu.sg', first_name: 'Choryi', last_name: 'Poon', password: '123123123')
+u2.confirm_email!
 ac = AccessCode.create(code: 'midterms', test_id: 1, permits: 5)
 cu = CodeUsage.create(access_code_id: ac.id, test_id: 1, email: "ckloh.2012@sis.smu.edu.sg", user_id: u.id)
 cu.use!(u)
