41c837af940c7ad41ffb57eb704c11e4a9933918
Mai Anh Vu <me@maianhvu.com>
2016-02-25 13:43:08 +0800

    add ApplicationController behaviour to expire ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :expire_hsts
   protect_from_forgery with: :exception
 
   protected
+
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  end
 
   def after_sign_in_path_for(resource)
     :dashboard_index
