4d5a2e009a2a91b885e5b9136bf55ca34fdc1b0e
Mai Anh Vu <me@maianhvu.com>
2016-03-10 23:12:05 +0800

    push changes to payment systems online for testing

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,29 @@
+class ChargesController < ApplicationController
+
+  PRICE_TEST_DEFAULT = 1500
+
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = PRICE_TEST_DEFAULT
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'sgd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
+end
