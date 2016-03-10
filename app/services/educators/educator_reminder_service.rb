0368acf4f3c64e388c867a8c12b3267e3fba3d76
Kia Yong <tankiayong91@gmail.com>
2016-03-10 18:15:50 +0800

    Check for batch before attempting to send batch completion email

--- app/services/educators/educator_reminder_service.rb
+++ app/services/educators/educator_reminder_service.rb
@@ -2,7 +2,7 @@ class Educators::EducatorReminderService
 
   def check_batch_completion(code_usage)
     batch = code_usage.batch
-    if batch.code_usages.all? { |cu| cu.complete? }
+    if batch && batch.code_usages.all? { |cu| cu.complete? }
       Educators::EducatorMailer.batch_completion_email(batch.educator, batch_name).deliver_now
     end
   end
