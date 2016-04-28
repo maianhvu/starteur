2cbd67d87d17cf3a37e404fc151c9030aabbf255
Kia Yong <tankiayong91@gmail.com>
2016-04-28 09:24:16 +0800

    Make organisational searches return only one result per organisation

--- app/controllers/educators/admin/search_controller.rb
+++ app/controllers/educators/admin/search_controller.rb
@@ -3,7 +3,7 @@ class Educators::Admin::SearchController < Educators::Admin::BaseController
   def index
     num_per_page = 10 # number of items to be displayed PER page
     if search_params.any?
-      @organisations = Kaminari.paginate_array(Educator.where("lower(organisation) like ?", "%#{search_params[:search_text].downcase}%").pluck(:organisation)).page(params[:org_page]).per(num_per_page)
+      @organisations = Kaminari.paginate_array(Educator.where("lower(organisation) like ?", "%#{search_params[:search_text].downcase}%").pluck(:organisation).uniq).page(params[:org_page]).per(num_per_page)
       @educator_emails = Educator.where("email like ? or lower(first_name) like ? or lower(last_name) like ?", "%#{search_params[:search_text]}%", "%#{search_params[:search_text].downcase}%", "%#{search_params[:search_text].downcase}%").page(params[:email_page]).per(num_per_page)
       @batches = Batch.where("lower(name) like ?", "%#{search_params[:search_text].downcase}%").page(params[:batch_page]).per(num_per_page)
       @users = User.where("email like ? or lower(first_name) like ? or lower(last_name) like ?", "%#{search_params[:search_text]}%", "%#{search_params[:search_text].downcase}%", "%#{search_params[:search_text].downcase}%").page(params[:user_page]).per(num_per_page)
