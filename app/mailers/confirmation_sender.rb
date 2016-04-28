6583e6703f82d8711b41c4e97bc4c10d94af17f2
Alex Foo <alexfoodw@gmail.com>
2016-04-28 17:31:13 +0800

    change back mailer to original

--- app/mailers/confirmation_sender.rb
+++ app/mailers/confirmation_sender.rb
@@ -8,21 +8,21 @@ class ConfirmationSender < ApplicationMailer
       :token => user.confirmation_token,
       :host => 'http://www.starteur.com'
     }
-    headers "X-SMTPAPI" => {
-      "sub" => {
-        "-headerText-" => ["Hi #{user.first_name}, thanks for registering with Starteur!"],
-        "-linkUrl-" => ["#{confirm_params[:host]}/confirm/#{confirm_params[:escaped_email]}/#{confirm_params[:token]}"],
-        "-linkCaption-" => ["Confirm email address"]
+    headers['X-SMTPAPI'] = {
+      sub: {
+      "-headerText-" => ["Hi #{user.first_name}, thanks for registering with Starteur!"],
+      "-linkUrl-" => ["#{confirm_params[:host]}/confirm/#{confirm_params[:escaped_email]}/#{confirm_params[:token]}"],
+      "-linkCaption-" => ["Confirm email address"]
       },
-      "filters" => {
-        "templates" => {
-          "settings" => {
-            "enable" => 1,
-            "template_id" => 'c8182604-b48b-46b9-aa25-65f340379519'
+      filters: {
+        templates: {
+           settings: {
+              enable: 1,
+              template_id: "c8182604-b48b-46b9-aa25-65f340379519"
+              }
+            }
           }
-        }
-      }
     }.to_json
-    mail(:to => @user.email, :subject => 'Welcome to Starteur! Confirm Your Email')
+   mail(:to => @user.email, :subject => 'Welcome to Starteur! Confirm Your Email')
   end
 end
